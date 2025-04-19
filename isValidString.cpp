bcf25c07efc10dcd70fd14d9a169aa9492200dba
Danny Regan <d.regan@columbia.edu>
2025-04-19 20:43:05 +0200

    Main function established.

---
+++ isValidString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int isValid(string input) {
+  return 0;
+}
+
+int main() {
+  string input;
+  cout << "Input a string. You can include '()' and '[]': ";
+  cin >> input;
+  isValid(input);
+  cout << input;
+  return 0;
+}
