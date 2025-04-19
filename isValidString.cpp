66cc6a3db1cc10de9caaf7cecb7de7c3a1918a9d
Danny Regan <d.regan@columbia.edu>
2025-04-19 22:00:58 +0200

    Program receives an input and determines if its valid based on the CFG and PDA.

--- isValidString.cpp
+++ isValidString.cpp
@@ -1,15 +1,67 @@
 #include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-int isValid(string input) {
-  return 0;
+bool isInSymbols(char c) {
+  vector<char> symbols = {'(', ')', '[', ']'};
+
+  for (int i = 0; i < symbols.size(); i++) {
+    if (c == symbols[i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool isInLanguage(char c) {
+  return isalpha(c) || isspace(c) || isInSymbols(c);
+}
+
+bool isValid(string input) {
+  int length = input.length();
+  stack<char> read;
+
+  for (int i = 0; i < length; i++) {
+    char c = input[i];
+
+    if (isInLanguage(c)) {
+      switch(c) {
+        case '[':
+          read.push(c);
+          break;
+
+        case ']':
+          if (read.top() == '[') {
+            read.pop();
+          }
+          else return false;
+          break;
+
+        case '(':
+          read.push(c);
+          break;
+
+        case ')':
+          if (read.top() == '(') {
+            read.pop();
+          }
+          else return false;
+          break;
+      }
+    }
+    else return false;
+  }
+  return read.empty();
 }
 
 int main() {
   string input;
   cout << "Input a string. You can include '()' and '[]': ";
-  cin >> input;
-  isValid(input);
-  cout << input;
+  getline(cin, input);
+  isValid(input) ? cout << "String is ACCEPTED" << endl : cout << "String is DENIED" << endl;
   return 0;
 }
